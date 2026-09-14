98485fd52a88c55ee7d5cc80f60373c2d695a8c7
Wen-Ming-Yuan <1941870298@qq.com>
2026-09-14 21:13:09 +0800

    Update usenix.py

--- sources/usenix.py
+++ sources/usenix.py
@@ -4,6 +4,18 @@ from bs4 import BeautifulSoup
 from sources.base import BaseSource
 from core.http import get_session
 
+UA = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+      "AppleWebKit/537.36 (KHTML, like Gecko) "
+      "Chrome/151.0.0.0 Safari/537.36")
+
+HEADERS = {
+    "User-Agent": UA,
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.usenix.org/",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+}
 
 class UsenixSource(BaseSource):
     name = "usenix"
